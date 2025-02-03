4ebc3a713d43167b7e2c4c6540625930dd394a56
ruos-sovanra <vannraruos@gmail.com>
2025-02-03 12:00:18 +0700

    update code

--- generated/main.tsx
+++
@@ -1,8 +0,0 @@
-\u002F\u002F basic example showing component with string passed as prop \u002F\u002F
-import React from 'react';
-
-const MyComponent = ({ str }) => {
-  return <h1>dsfdsfsdfsdf{str}</h1>;
-};
-
-export default MyComponent;
